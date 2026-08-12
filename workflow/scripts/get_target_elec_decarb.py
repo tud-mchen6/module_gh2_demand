b0dbcf3870faff234826bd22888c6bee482610e2
tud-mchen6 <m.chen-4@tudelft.nl>
2026-08-12 11:47:46 +0200

    change the multiplication to addition logic in elec/decarb rate increase

--- workflow/scripts/get_target_elec_decarb.py
+++ workflow/scripts/get_target_elec_decarb.py
@@ -9,20 +9,20 @@ def scale_or_initialise(entry, relative, init):
     in which case return the initialisation value.
     """
     if entry > 0:
-        return min(relative * entry, 1)
+        return min(relative + entry, 1)
     else:
         return init
 
 
 def get_target_elec_decarb(
-        countries : list, 
-        use_historical : bool, 
-        elec_decarb_rel : float,
-        elec_decarb_in_case_zero : float,
-        sector_overwrite_dir : str,
-        reference_sector_elec_decarb : str,
-        output_file : str,
-        country_specific_overwrite : str = None,
+    countries: list,
+    use_historical: bool,
+    elec_decarb_rel: float,
+    elec_decarb_in_case_zero: float,
+    sector_overwrite_dir: str,
+    reference_sector_elec_decarb: str,
+    output_file: str,
+    country_specific_overwrite: str = None,
 ):
     """
     Get the target decarbonisation level for electricity for each country,
@@ -32,11 +32,11 @@ def get_target_elec_decarb(
 
     Parameters:
     - countries: list - List of country codes to process, given in ISO3 codes.
-    - use_historical: bool - Whether to directly use historical values in the 
+    - use_historical: bool - Whether to directly use historical values in the
         reference year.
-    - elec_decarb_rel: float - Decarbonisation of electricity relative to historical ones 
+    - elec_decarb_rel: float - Decarbonisation of electricity relative (additional) to historical ones
         in case that hte historical ones are non-zero.
-    - elec_decarb_in_case_zero: float - Decarbonisation of electricity assumed for sectors 
+    - elec_decarb_in_case_zero: float - Decarbonisation of electricity assumed for sectors
         where historically the rate is zero.
     - sector_overwrite_dir: str - Path of the directory to store any user-given
         overwrite files.
@@ -47,74 +47,105 @@ def get_target_elec_decarb(
     """
 
     # Apply the config default values
-    sector = 'ELEC_DECARB'
-    sectors = ['TOTIND', 'TOTTRANS', 'RESIDENT', 'COMMPUB', 'AGRICULT', 'FISHING', 'ONONSPEC', 'NONENUSE']
+    sector = "ELEC_DECARB"
+    sectors = [
+        "TOTIND",
+        "TOTTRANS",
+        "RESIDENT",
+        "COMMPUB",
+        "AGRICULT",
+        "FISHING",
+        "ONONSPEC",
+        "NONENUSE",
+    ]
     reference = pd.read_csv(reference_sector_elec_decarb, index_col=0)[[sector]]
     reference_df = pd.DataFrame(columns=sectors, index=reference.index)
     reference_df[:] = reference.values
 
     if not use_historical:
         target_df = reference_df.copy().map(
-            lambda x: scale_or_initialise(x, elec_decarb_rel, elec_decarb_in_case_zero))
+            lambda x: scale_or_initialise(x, elec_decarb_rel, elec_decarb_in_case_zero)
+        )
 
         # Get all country specific overwrites (low priority, higher than config default)
         if country_specific_overwrite is not None:
-            country_level_overwrite_df = pd.read_csv(country_specific_overwrite, index_col=0)
+            country_level_overwrite_df = pd.read_csv(
+                country_specific_overwrite, index_col=0
+            )
             for country in list(country_level_overwrite_df.index):
                 # Users could choose to determine directly an absolute decarbonisation level,
                 # or to define a relative increase compared to the current value, that is different
                 # from that defined in config. But these two cannot exist at the same time.
-                abs = country_level_overwrite_df.loc[country, 'elec_decarb_abs']
-                rel = country_level_overwrite_df.loc[country, 'elec_decarb_rel']
+                abs = country_level_overwrite_df.loc[country, "elec_decarb_abs"]
+                rel = country_level_overwrite_df.loc[country, "elec_decarb_rel"]
                 if pd.notna(abs) and pd.notna(rel):
-                    print("Input error: relative values and absolute values cannot be given at the same time for " \
-                    "electricity decarbonisation level.")
+                    print(
+                        "Input error: relative values and absolute values cannot be given at the same time for "
+                        "electricity decarbonisation level."
+                    )
                 elif pd.notna(abs):
                     target_df.loc[country] = abs
                     if reference_df.at[country, sector] > abs:
-                        print("Input warning: given electricity decarbonisation level lower than historical value"
-                                                f" in {sector} sector in {country}.")
+                        print(
+                            "Input warning: given electricity decarbonisation level lower than historical value"
+                            f" in {sector} sector in {country}."
+                        )
                 elif pd.notna(rel):
-                    target_df.loc[country] = reference_df.loc[country].apply(lambda x: scale_or_initialise(x, 
-                                                                rel,
-                                                                elec_decarb_in_case_zero))
+                    target_df.loc[country] = reference_df.loc[country].apply(
+                        lambda x: scale_or_initialise(x, rel, elec_decarb_in_case_zero)
+                    )
                 else:
                     continue
 
-
         # Get all sector specific overwrites (highest priority)
-        all_files = glob.glob(os.path.join(sector_overwrite_dir,"*.csv"))
+        all_files = glob.glob(os.path.join(sector_overwrite_dir, "*.csv"))
         for file in all_files:
             # Only get the ones with the naming convention; hard-coded
-            file_name = file.replace(sector_overwrite_dir, "").replace(".csv","").replace("\\","")
-            if 'sectorShare' in file_name:
+            file_name = (
+                file.replace(sector_overwrite_dir, "")
+                .replace(".csv", "")
+                .replace("\\", "")
+            )
+            if "sectorShare" in file_name:
                 country = file_name.split("_")[-1]
                 # Only get the ones where country is within the countries list
                 if country in countries:
-                    country_sector_specific_overwrite = pd.read_csv(file,index_col=0)
+                    country_sector_specific_overwrite = pd.read_csv(file, index_col=0)
                     # Check if the value that the user gives is coherent. If not,
                     # fall back to the default
                     for sector in list(country_sector_specific_overwrite.index):
-                        abs = country_sector_specific_overwrite.loc[sector, 'elec_decarb_abs']
-                        rel = country_sector_specific_overwrite.loc[sector, 'elec_decarb_rel']
+                        abs = country_sector_specific_overwrite.loc[
+                            sector, "elec_decarb_abs"
+                        ]
+                        rel = country_sector_specific_overwrite.loc[
+                            sector, "elec_decarb_rel"
+                        ]
                         if pd.notna(abs) and pd.notna(rel):
-                            print("Input error: relative values and absolute values cannot be given at the same time for " \
-                                "electricity decarbonisation level.")
+                            print(
+                                "Input error: relative values and absolute values cannot be given at the same time for "
+                                "electricity decarbonisation level."
+                            )
                         elif pd.notna(abs):
                             target_df.at[country, sector] = abs
                             if reference_df.at[country, sector] > abs:
-                                print("Input warning: given electricity decarbonisation level lower than historical value"
-                                        f" in {sector} sector in {country}.")
+                                print(
+                                    "Input warning: given electricity decarbonisation level lower than historical value"
+                                    f" in {sector} sector in {country}."
+                                )
                         elif pd.notna(rel):
-                            target_df.at[country, sector] = scale_or_initialise(reference_df.at[country, sector], 
-                                                                                rel,
-                                                                                elec_decarb_in_case_zero)
+                            target_df.at[country, sector] = scale_or_initialise(
+                                reference_df.at[country, sector],
+                                rel,
+                                elec_decarb_in_case_zero,
+                            )
                         else:
                             continue
-                elif country=="template":
+                elif country == "template":
                     continue
                 else:
-                    print("Input error: given country not present in analysed countries.")
+                    print(
+                        "Input error: given country not present in analysed countries."
+                    )
         target_df = target_df.sort_index().round(4)
     else:
         target_df = reference_df
@@ -134,4 +165,3 @@ if __name__ == "__main__":
         reference_sector_elec_decarb=snakemake.input.reference_sector_elec_decarb,
         output_file=snakemake.output.output_file,
     )
-
--- workflow/scripts/get_target_elec_rate.py
+++ workflow/scripts/get_target_elec_rate.py
@@ -5,20 +5,20 @@ import os
 
 def scale_or_initialise(entry, relative, init):
     if entry > 0:
-        return min(relative * entry, 1)
+        return min(relative + entry, 1)
     else:
         return init
 
 
 def get_target_elec_rate(
-        countries : list, 
-        use_historical : bool,
-        sector_elec_rate_rel : float,
-        elec_rate_in_case_zero : float,
-        sector_overwrite_dir : str,
-        reference_sector_elec_rate : str,
-        output_file : str,
-        country_specific_overwrite : str = None,
+    countries: list,
+    use_historical: bool,
+    sector_elec_rate_rel: float,
+    elec_rate_in_case_zero: float,
+    sector_overwrite_dir: str,
+    reference_sector_elec_rate: str,
+    output_file: str,
+    country_specific_overwrite: str = None,
 ):
     """
     Get the target sectoral electrification rate for each country,
@@ -28,9 +28,9 @@ def get_target_elec_rate(
 
     Parameters:
     - countries: list - List of country codes to process, given in ISO3 codes.
-    - use_historical: bool - Whether to directly use historical values in the 
+    - use_historical: bool - Whether to directly use historical values in the
         reference year.
-    - sector_elec_rate_rel: float - Electrification rate relative to historical ones 
+    - sector_elec_rate_rel: float - Electrification rate relative (additional) to historical ones
         in case that hte historical ones are non-zero.
     - elec_rate_in_case_zero: float - Electrificaiton rate assumed for sectors where
         historically the rate is zero.
@@ -46,71 +46,99 @@ def get_target_elec_rate(
     reference_df = pd.read_csv(reference_sector_elec_rate, index_col=0)
     if not use_historical:
         # If not use the historical values, go for the config-defined defaults
+        breakpoint()
         target_df = reference_df.copy().map(
-            lambda x: scale_or_initialise(x, sector_elec_rate_rel, elec_rate_in_case_zero))
+            lambda x: scale_or_initialise(
+                x, sector_elec_rate_rel, elec_rate_in_case_zero
+            )
+        )
 
         # Get all country specific overwrites (low priority, higher than config default)
         if country_specific_overwrite is not None:
-            country_level_overwrite_df = pd.read_csv(country_specific_overwrite, index_col=0)
+            country_level_overwrite_df = pd.read_csv(
+                country_specific_overwrite, index_col=0
+            )
             for country in list(country_level_overwrite_df.index):
                 # Users could choose to determine directly an absolute electrification rate,
                 # or to define a relative increase compared to the current value, that is different
                 # from that defined in config. But these two cannot exist at the same time.
-                abs = country_level_overwrite_df.loc[country, 'sector_elec_rate_abs']
-                rel = country_level_overwrite_df.loc[country, 'sector_elec_rate_rel']
+                abs = country_level_overwrite_df.loc[country, "sector_elec_rate_abs"]
+                rel = country_level_overwrite_df.loc[country, "sector_elec_rate_rel"]
                 if pd.notna(abs) and pd.notna(rel):
-                    print("Input error: relative values and absolute values cannot be given at the same time for " \
-                    "electrification rate.")
+                    print(
+                        "Input error: relative values and absolute values cannot be given at the same time for "
+                        "electrification rate."
+                    )
                 elif pd.notna(abs):
                     target_df.loc[country] = abs
                     for sector in reference_df.columns:
                         if reference_df.at[country, sector] > abs:
-                            print("Input warning: given electrification lower than historical value"
-                                                f" in {sector} sector in {country}.")
+                            print(
+                                "Input warning: given electrification lower than historical value"
+                                f" in {sector} sector in {country}."
+                            )
                 elif pd.notna(rel):
-                    target_df.loc[country] = reference_df.loc[country].apply(lambda x: scale_or_initialise(x, 
-                                                                rel,
-                                                                elec_rate_in_case_zero))
+                    target_df.loc[country] = reference_df.loc[country].apply(
+                        lambda x: scale_or_initialise(x, rel, elec_rate_in_case_zero)
+                    )
                 else:
                     continue
 
-
         # Get all sector specific overwrites (highest priority)
-        all_files = glob.glob(os.path.join(sector_overwrite_dir,"*.csv"))
+        all_files = glob.glob(os.path.join(sector_overwrite_dir, "*.csv"))
         for file in all_files:
             # Only get the ones with the naming convention; hard-coded
-            file_name = file.replace(sector_overwrite_dir, "").replace(".csv","").replace("\\","")
-            if 'sectorShare' in file_name:
+            file_name = (
+                file.replace(sector_overwrite_dir, "")
+                .replace(".csv", "")
+                .replace("\\", "")
+            )
+            if "sectorShare" in file_name:
                 country = file_name.split("_")[-1]
                 # Only get the ones where country is within the countries list
                 if country in countries:
-                    country_sector_specific_overwrite = pd.read_csv(file,index_col=0)
+                    country_sector_specific_overwrite = pd.read_csv(file, index_col=0)
                     # Check if the value that the user gives is coherent. If not,
                     # fall back to the default
                     for sector in list(country_sector_specific_overwrite.index):
-                        abs = country_sector_specific_overwrite.loc[sector, 'sector_elec_rate_abs']
-                        rel = country_sector_specific_overwrite.loc[sector, 'sector_elec_rate_rel']
+                        abs = country_sector_specific_overwrite.loc[
+                            sector, "sector_elec_rate_abs"
+                        ]
+                        rel = country_sector_specific_overwrite.loc[
+                            sector, "sector_elec_rate_rel"
+                        ]
                         if pd.notna(abs) and pd.notna(rel):
-                            print("Input error: relative values and absolute values cannot be given at the same time for " \
-                                "electrification rate.")
+                            print(
+                                "Input error: relative values and absolute values cannot be given at the same time for "
+                                "electrification rate."
+                            )
                         elif pd.notna(abs):
                             target_df.at[country, sector] = abs
                             if reference_df.at[country, sector] > abs:
-                                print("Input warning: given electrification lower than historical value"
-                                        f" in {sector} sector in {country}.")
+                                print(
+                                    "Input warning: given electrification lower than historical value"
+                                    f" in {sector} sector in {country}."
+                                )
                         elif pd.notna(rel):
-                            target_df.at[country, sector] = scale_or_initialise(reference_df.at[country, sector], 
-                                                                                rel,
-                                                                                elec_rate_in_case_zero)
+                            target_df.at[country, sector] = scale_or_initialise(
+                                reference_df.at[country, sector],
+                                rel,
+                                elec_rate_in_case_zero,
+                            )
                         else:
                             continue
-                elif country=="template":
+                elif country == "template":
                     continue
                 else:
-                    print("Input error: given country not present in analysed countries.")
+                    print(
+                        "Input error: given country not present in analysed countries."
+                    )
         target_df = target_df.sort_index().round(4)
     else:
         target_df = reference_df
+
+    # Clean the output file; non-energy use of other carriers cannot be electrified
+    target_df["NONENUSE"] = 0
 
     # Output the file
     target_df.to_csv(output_file)
@@ -127,4 +155,3 @@ if __name__ == "__main__":
         output_file=snakemake.output.output_file,
         country_specific_overwrite=snakemake.params.country_overwrite,
     )
-
